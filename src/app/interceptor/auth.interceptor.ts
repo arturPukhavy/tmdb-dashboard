5f4c8631d97601939216b5da9b2293dae7d91d36
Artur Pukhavoi <arturpuhice@gmail.com>
2024-10-28 20:14:32 +0300

    add logo

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -5,14 +5,19 @@ import { environment } from '../../environments/environment';
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const apiKey = environment.apiKey;
 
-  // Clone the request to add the new headers
-  const clonedRequest = req.clone({
-    setHeaders: {
-      'Authorization': `Bearer ${apiKey}`,
-      'accept': 'application/json',
-    }
-  });
+  if (apiKey !== 'secret' && apiKey.trim() !== '') {
+    // Clone the request to add the new headers
+    const clonedRequest = req.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${apiKey}`,
+        'accept': 'application/json',
+      }
+    });
 
-  // Pass the modified request to the next handler in the chain
-  return next(clonedRequest);
+    // Pass the modified request to the next handler in the chain
+    return next(clonedRequest);
+  }
+
+  // If apiKey is 'secret' or empty, just pass the original request
+  return next(req);
 };
