d9501cd5a24c4dfcd5510d239b4117bdc4e64427
Artur Pukhavoi <arturpuhice@gmail.com>
2024-11-06 21:04:42 +0300

    mobile responsive style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CommonModule, Location } from '@angular/common';
 import { HeaderComponent } from './shared/header/header.component';
@@ -11,17 +11,32 @@ import { HeaderComponent } from './shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showBackButton = true;
 
-  constructor(private location: Location, private router: Router) {
-    this.router.events.subscribe((event: any) => {
-      if (event.url !== '/' && event.url !== '/home') {
-        this.showBackButton = true; // Don't show the button on the first page
-      } else {
-        this.showBackButton = false;
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit() {
+    this.updateBackButtonVisibility(); // Initial check
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateBackButtonVisibility(); // Check route after navigation
       }
     });
+  }
+
+  // Listen to window resize events and update the button visibility
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateBackButtonVisibility();
+  }
+
+  updateBackButtonVisibility(): void {
+    const isMobile = window.innerWidth <= 768; // Mobile screen size
+    const isHomePage = this.router.url === '/' || this.router.url === '/home';
+
+    // Show back button only if not on mobile and not on home page
+    this.showBackButton = !isMobile && !isHomePage;
   }
 
   goBack(): void {
